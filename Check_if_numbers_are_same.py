a06c9fb7cc745ca05b7a3aec6757a012864b177e
jiekai xu <jacobhsu1994@gmail.com>
2017-12-04 20:46:13 -0800

    Make some changes

--- Check_if_numbers_are_same.py
+++ Check_if_numbers_are_same.py
@@ -1,17 +1,35 @@
+
 #This program takes a list of numbers and determines if they are all mutually exclusive from each other
 
 print("This program takes a list of numbers and determines if they are all mutually exclusive from each other.")
 
-list = input("Enter a list of numbers in this syntax: [a,b,c,d,e]- ")
-
 def main(data):
-    count = 0
+    copy = []
+    # iterate all data
     for k in data:
-        for j in range (1, len(data) - 1):
-            if k == j:
-                count += 1
-                if count == 2:
-                    return False
+        # if k is ',' do nothing, because ',' is not the data that we want to check
+        if k == ',':
+            pass
+        else:
+            # iterate the copy list
+            for j in copy:
+                # if k is in copy, then it has duplicate return true to exit function
+                if k == j:
                     return True
-            
-print(list)
+            # if after the iteration of data, it means k is not in copy,
+            # then store it in copy for later check
+            copy.append(k)
+    # duplicate not found, return false
+    return False
+
+# loop the prompt
+while True:
+    list = input("Enter a list of numbers in this syntax: [a,b,c,d,e]-, Enter q to quit: ")
+    # if q then quit
+    if list == 'q': 
+        break
+    print(list)
+    if main(list) == True:
+        print('Elements in the list are not mutually exclusive from each other')
+    else:
+        print('Elements in the list are mutaully exclusive from each other')
